ea4f4cf7287d1ce35a98b151944e1d5f5cde160f
aminjonshermatov <aminjonshermatov@gmail.com>
2022-09-23 23:04:53 +0300

    8/5

--- gruber_tasks/chapter_8/5.sql
+++ gruber_tasks/chapter_8/5.sql
@@ -6,8 +6,7 @@ alter session set current_schema = AMINJON;
 --     и ставку комиссионных продавца.
 
 
-select C.CNAME, S.SNAME, S.COMM * O.AMT as commission
-from ORDERS O
-         left join SALESPEOPLE S on O.SNUM = S.SNUM
-         left join CUSTOMERS C on O.CNUM = C.CNUM
+select C.CNAME, S.SNAME, S.COMM
+from CUSTOMERS C
+         left join SALESPEOPLE S on C.SNUM = S.SNUM
 where S.COMM > .12;
