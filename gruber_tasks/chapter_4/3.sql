15afb9c9df8e7a7532aa399fdd10dedf6001bfef
aminjonshermatov <aminjonshermatov@gmail.com>
2022-09-23 21:00:19 +0300

    4/3

---
+++ gruber_tasks/chapter_4/3.sql
@@ -0,0 +1,6 @@
+alter session set current_schema = AMINJON;
+
+-- Напишите запрос который может выдать вам поля sname и city для всех продавцов в Лондоне с комиссионными выше .10 .
+
+select * from SALESPEOPLE
+where lower(CITY) = 'london' and COMM > .1;
