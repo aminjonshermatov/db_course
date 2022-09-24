c6973a3bd7b14580f45ccfac6ee6f4d4738308ad
aminjonshermatov <aminjonshermatov@gmail.com>
2022-09-24 13:19:09 +0300

    10/5

---
+++ gruber_tasks/chapter_10/5.sql
@@ -0,0 +1,11 @@
+alter session set current_schema = AMINJON;
+
+-- Напишите запрос который бы выбрал общую сумму всех  приобретений  в
+--    заказах  для  каждого продавца,  у которого эта общая сумма больше
+--    чем наибольшая стоимость заказов в таблице.
+
+
+select o.SNUM, sum(o.AMT) as sum_
+from ORDERS o
+group by o.SNUM
+having sum(o.AMT) > max(o.AMT);
